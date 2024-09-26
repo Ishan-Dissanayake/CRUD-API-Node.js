2cb39ce717dda189c4edc2ad24054d69e4e78a11
Ishan-Dissanayake <ishandissanayaka00@gmail.com>
2024-09-26 14:32:34 +0530

    CRU

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,56 @@
+
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import {BrowserRouter , Routes , Route} from 'react-router-dom'
+import Student from './Student.jsx'
+import React from 'react';
+import CreateStudent from './CreateStudent.jsx';
+import UpdateStudent from './UpdateStudent.jsx';
+
+
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+<BrowserRouter>
+<Routes>
+  <Route path='/' element={<Student />}></Route>
+  <Route path='/create' element={<CreateStudent />}></Route>
+  <Route path='/update/:id' element={<UpdateStudent />}></Route>
+</Routes>
+</BrowserRouter>
+    </div>
+
+
+
+
+
+
+    // <>
+    //   <div>
+    //     <a href="https://vitejs.dev" target="_blank">
+    //       <img src={viteLogo} className="logo" alt="Vite logo" />
+    //     </a>
+    //     <a href="https://react.dev" target="_blank">
+    //       <img src={reactLogo} className="logo react" alt="React logo" />
+    //     </a>
+    //   </div>
+    //   <h1>Vite + React</h1>
+    //   <div className="card">
+    //     <button onClick={() => setCount((count) => count + 1)}>
+    //       count is {count}
+    //     </button>
+    //     <p>
+    //       Edit <code>src/App.jsx</code> and save to test HMR
+    //     </p>
+    //   </div>
+    //   <p className="read-the-docs">
+    //     Click on the Vite and React logos to learn more
+    //   </p>
+    // </>
+  )
+}
+
+export default App
