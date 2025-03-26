aa8162a16dff495259d93c8e79318bd5c230d818
Ishan-Dissanayake <ishandissanayaka00@gmail.com>
2025-03-27 02:41:12 +0530

    s

---
+++ backend/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string name = "umanda";
+        int order = 3;
+        string completeName = $"{name} the {order}rd";
+
+        Console.WriteLine(completeName);
+    }
+}
