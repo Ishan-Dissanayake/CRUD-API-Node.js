6bd9ab90eadf2d84e48cb1a33d3d4cb40008660b
Ishan-Dissanayake <ishandissanayaka00@gmail.com>
2025-03-27 02:42:47 +0530

    s

--- vite-project/src/Student.jsx
+++ vite-project/src/Student.jsx
@@ -22,7 +22,7 @@ function Student() {
     }
 
 
-//home sxsssasss
+//home sxsssasssa
     return (
         <div className='d-flex vh-100 vw-100 bg-primary justify-content-center align-items-center'>
           
